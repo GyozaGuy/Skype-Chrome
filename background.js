c2af2364ab5d4f6371e37aaf6b5bab40b23b0583
Steven Stromberg <steven.stromberg@intel.com>
2015-10-29 10:39:51 -0600

    Window stays centered

--- background.js
+++ background.js
@@ -1,6 +1,17 @@
+var height = 650;
+var width = 1000;
 chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create(
-	  'index.htm',
-	  {id: 'skype', height: 650, width: 1000}
-	);
+	chrome.app.window.create('index.htm', {
+		id: 'skype',
+		outerBounds: {
+	    height: height,
+	    width: width
+	  }
+	},
+	function(win) {
+    win.outerBounds.setPosition(
+      Math.round(screen.availWidth / 2 - width / 2),
+      Math.round(screen.availHeight / 2 - height / 2)
+    );
+	});
 });
